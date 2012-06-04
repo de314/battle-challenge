da4af3821364c1ce242261ce284b8cab91c674cb
David Esposito <desposito6@gatech.edu>
2012-06-04 07:34:25 -0400

    committing server skeleton code

---
+++ src/battlechallenge/server/NetworkConnection.java
@@ -0,0 +1,31 @@
+package battlechallenge.server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class NetworkConnection {
+
+	private Socket conn;
+	private ObjectOutputStream oos;
+	private ObjectInputStream ois;
+	
+	public NetworkConnection() {
+		// TODO
+	}
+	
+	public void setupHandshake() {
+		// TODO
+	}
+	
+	public void setPlayerStatus() {
+		// TODO
+	}
+	
+	public void placeShips() {
+		// TODO
+	}
+	
+	public void doTurn() {
+		// TODO
+	}
+}
---
+++ src/battlechallenge/server/Player.java
@@ -0,0 +1,26 @@
+package battlechallenge.server;
+
+public class Player {
+
+	private String name;
+	private Board board;
+	private NetworkConnection conn;
+	
+	public Player() {
+		// TODO
+	}
+	
+	public void placeShips(List<Ship> ships) {
+		// TODO
+	}
+	
+	public boolean requestTurn() {
+		// TODO
+		return false;
+	}
+	
+	public Coordinate getTurn() {
+		// TODO
+	}
+	
+}
