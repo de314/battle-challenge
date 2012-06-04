143407c7589303531b600308c406537b9c888d9f
David Esposito <desposito6@gatech.edu>
2012-06-04 07:08:15 -0400

    small commit 3

---
+++ src/battlechallenge/server/GameManager.java
@@ -0,0 +1,13 @@
+package battlechallenge.server;
+
+import java.util.List;
+
+import battlechallenge.bot.Player;
+import battlechallenge.client.Game;
+
+public class GameManager {
+
+	private List<Game> games; // FIXME: this needs to be battlechallenge.server.Game
+	private List<Player> players;
+	
+}
