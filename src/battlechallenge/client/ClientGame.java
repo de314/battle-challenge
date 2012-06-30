d29cbe3439840df81a000f037596fba9d3f1d393
David Esposito <desposito6@gatech.edu>
2012-06-29 21:40:23 -0400

    Adding skeleton client game service provider

---
+++ src/battlechallenge/client/ClientGame.java
@@ -0,0 +1,43 @@
+package battlechallenge.client;
+
+import java.util.List;
+
+import battlechallenge.ActionResult;
+import battlechallenge.ship.Ship;
+import battlechallenge.structures.Base;
+import battlechallenge.structures.City;
+
+public class ClientGame {
+	
+	public List<Ship> getMyShips() {
+		return null;
+	}
+	
+	public List<List<Ship>> getOpponentShips() {
+		return null;
+	}
+
+	public List<ActionResult> getMyActionResults() {
+		return null;
+	}
+	
+	public List<List<ActionResult>> getOpponentActionResults() {
+		return null;
+	}
+	
+	public Base getMyBase() {
+		return null;
+	}
+	
+	public List<Base> getOponnentBases() {
+		return null;
+	}
+	
+	public List<City> getMyCities() {
+		return null;
+	}
+	
+	public List<City> getAllCities() {
+		return null;
+	}
+}
